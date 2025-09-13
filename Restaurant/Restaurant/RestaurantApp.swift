24115d8b02fdf9f416e2d43c3586090b2c0070b7
dlarenas <dario_chamo_chamo@hotmail.com>
2025-09-13 14:03:18 -0500

    message

---
+++ Restaurant/Restaurant/RestaurantApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantApp.swift
+//  Restaurant
+//
+//  Created by Dario Larenas on 13/9/25.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
