23556ec624d2026fbf2926475095d5c69491dd99
dlarenas <dario_chamo_chamo@hotmail.com>
2025-09-19 21:32:46 -0500

    Fix Scroll View

--- Restaurant/Restaurant/UserProfile.swift
+++ Restaurant/Restaurant/UserProfile.swift
@@ -25,186 +25,188 @@ struct UserProfile: View {
     @Environment(\.presentationMode) var presentation
     
     var body: some View {
-        VStack {
-            TopBarUserProfile(selectedTab: $selectedTab)
-                .padding(.bottom, 10)
-            Text("Personal information")
-                .font(.headline)
-                .frame(maxWidth: .infinity, alignment: .leading)
-            HStack {
-                
-                Image("profile-image-placeholder")
-                    .resizable()
-                    .frame(width: 66, height: 66)
-                Button(action: {
-                    //selectedChange.toggle()
-                }) {
-                    Text("Change")
-                        .font(.system(size: 16, weight: .medium))
+        ScrollView {
+            VStack {
+                TopBarUserProfile(selectedTab: $selectedTab)
+                    .padding(.bottom, 10)
+                Text("Personal information")
+                    .font(.headline)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                HStack {
+                    
+                    Image("profile-image-placeholder")
+                        .resizable()
+                        .frame(width: 66, height: 66)
+                    Button(action: {
+                        //selectedChange.toggle()
+                    }) {
+                        Text("Change")
+                            .font(.system(size: 16, weight: .medium))
                         //.foregroundColor(selectedChange ? Color.white : Color(red: 0.286, green: 0.369, blue: 0.341))
-                        .foregroundColor(Color.white)
-                        .padding(.vertical, 8)
-                        .padding(.horizontal, 16)
-                        .background(
-                            RoundedRectangle(cornerRadius: 8)
+                            .foregroundColor(Color.white)
+                            .padding(.vertical, 8)
+                            .padding(.horizontal, 16)
+                            .background(
+                                RoundedRectangle(cornerRadius: 8)
                                 //.fill(selectedChange ? Color(red: 0.286, green: 0.369, blue: 0.341) /*charcoal*/ : Color.white)
-                                .fill(Color(red: 0.286, green: 0.369, blue: 0.341) /*charcoal*/)
-                                .stroke(Color(red: 0.286, green: 0.369, blue: 0.341), lineWidth: 1)
-                        )
-                }
-                .padding(.leading, 12)
-                Button(action: {
-                    selectedRemove.toggle()
-                }) {
-                    Text("Remove")
-                        .font(.system(size: 16, weight: .medium))
+                                    .fill(Color(red: 0.286, green: 0.369, blue: 0.341) /*charcoal*/)
+                                    .stroke(Color(red: 0.286, green: 0.369, blue: 0.341), lineWidth: 1)
+                            )
+                    }
+                    .padding(.leading, 12)
+                    Button(action: {
+                        selectedRemove.toggle()
+                    }) {
+                        Text("Remove")
+                            .font(.system(size: 16, weight: .medium))
                         //.foregroundColor(selectedRemove ? Color.white : Color(red: 0.286, green: 0.369, blue: 0.341))
-                        .foregroundColor(Color(red: 0.286, green: 0.369, blue: 0.341))
-                        .padding(.vertical, 8)
-                        .padding(.horizontal, 16)
-                        .background(
-                            RoundedRectangle(cornerRadius: 8)
+                            .foregroundColor(Color(red: 0.286, green: 0.369, blue: 0.341))
+                            .padding(.vertical, 8)
+                            .padding(.horizontal, 16)
+                            .background(
+                                RoundedRectangle(cornerRadius: 8)
                                 //.fill(selectedRemove ? Color(red: 0.286, green: 0.369, blue: 0.341) /*charcoal*/ : Color.white)
-                                .fill(Color.white)
-                                .stroke(Color(red: 0.286, green: 0.369, blue: 0.341), lineWidth: 1)
+                                    .fill(Color.white)
+                                    .stroke(Color(red: 0.286, green: 0.369, blue: 0.341), lineWidth: 1)
+                            )
+                    }.padding(.leading, 12)
+                    Spacer()
+                }
+                
+                VStack(spacing: 15) {
+                    Text("First Name")
+                        .font(.system(size: 12, weight: .bold))
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.bottom, -10)
+                    
+                    TextField("First Name", text: $firstName)
+                        .font(.system(size: 15))
+                        .padding(.vertical, 10)
+                        .padding(.horizontal, 8)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 6)
+                                .stroke(Color.gray, lineWidth: 1)
                         )
-                }.padding(.leading, 12)
-                Spacer()
-            }
-            
-            VStack(spacing: 15) {
-                Text("First Name")
-                    .font(.system(size: 12, weight: .bold))
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding(.bottom, -10)
-                
-                TextField("First Name", text: $firstName)
-                    .font(.system(size: 15))
-                    .padding(.vertical, 10)
-                    .padding(.horizontal, 8)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 6)
-                            .stroke(Color.gray, lineWidth: 1)
-                    )
-                
-                Text("Last Name")
-                    .font(.system(size: 12, weight: .bold))
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding(.bottom, -10)
-                
-                TextField("Last Name", text: $lastName)
-                    .font(.system(size: 15))
-                    .padding(.vertical, 10)
-                    .padding(.horizontal, 8)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 6)
-                            .stroke(Color.gray, lineWidth: 1)
-                    )
-                
-                Text("Email")
-                    .font(.system(size: 12, weight: .bold))
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding(.bottom, -10)
-                
-                TextField("Email", text: $email)
-                    .keyboardType(.emailAddress)
-                    .autocapitalization(.none)
-                    .disableAutocorrection(true)
-                    .textInputAutocapitalization(.never)
-                    .font(.system(size: 15))
-                    .padding(.vertical, 10)
-                    .padding(.horizontal, 8)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 6)
-                            .stroke(Color.gray, lineWidth: 1)
-                    )
-                Text("Phone Number")
-                    .font(.system(size: 12, weight: .bold))
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding(.bottom, -10)
-                
-                TextField("Phone Number", text: $phone)
-                    .keyboardType(.phonePad)
-                    .autocapitalization(.none)
-                    .disableAutocorrection(true)
-                    .textInputAutocapitalization(.never)
-                    .font(.system(size: 15))
-                    .padding(.vertical, 10)
-                    .padding(.horizontal, 8)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 6)
-                            .stroke(Color.gray, lineWidth: 1)
-                    )
-                
-                Text("Email notifications")
-                    .font(.system(size: 15, weight: .bold))
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                
-                CustomCheckBox(isChecked: $isOrdersStatuses, label: "Order statuses")
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                CustomCheckBox(isChecked: $isPasswordChanges, label: "Password changes")
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                CustomCheckBox(isChecked: $isSpecialOffers, label: "Special offers")
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                CustomCheckBox(isChecked: $isNewsletter, label: "Newsletter")
-                    .frame(maxWidth: .infinity, alignment: .leading)
-            }
-            Button(action: {
-                UserDefaults.standard.set(false, forKey: kIsLoggedIn)
-                lougout()
-                presentation.wrappedValue.dismiss()
-            }
+                    
+                    Text("Last Name")
+                        .font(.system(size: 12, weight: .bold))
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.bottom, -10)
+                    
+                    TextField("Last Name", text: $lastName)
+                        .font(.system(size: 15))
+                        .padding(.vertical, 10)
+                        .padding(.horizontal, 8)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 6)
+                                .stroke(Color.gray, lineWidth: 1)
+                        )
+                    
+                    Text("Email")
+                        .font(.system(size: 12, weight: .bold))
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.bottom, -10)
+                    
+                    TextField("Email", text: $email)
+                        .keyboardType(.emailAddress)
+                        .autocapitalization(.none)
+                        .disableAutocorrection(true)
+                        .textInputAutocapitalization(.never)
+                        .font(.system(size: 15))
+                        .padding(.vertical, 10)
+                        .padding(.horizontal, 8)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 6)
+                                .stroke(Color.gray, lineWidth: 1)
+                        )
+                    Text("Phone Number")
+                        .font(.system(size: 12, weight: .bold))
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.bottom, -10)
+                    
+                    TextField("Phone Number", text: $phone)
+                        .keyboardType(.phonePad)
+                        .autocapitalization(.none)
+                        .disableAutocorrection(true)
+                        .textInputAutocapitalization(.never)
+                        .font(.system(size: 15))
+                        .padding(.vertical, 10)
+                        .padding(.horizontal, 8)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 6)
+                                .stroke(Color.gray, lineWidth: 1)
+                        )
+                    
+                    Text("Email notifications")
+                        .font(.system(size: 15, weight: .bold))
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    
+                    CustomCheckBox(isChecked: $isOrdersStatuses, label: "Order statuses")
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    CustomCheckBox(isChecked: $isPasswordChanges, label: "Password changes")
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    CustomCheckBox(isChecked: $isSpecialOffers, label: "Special offers")
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    CustomCheckBox(isChecked: $isNewsletter, label: "Newsletter")
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                }
+                Button(action: {
+                    UserDefaults.standard.set(false, forKey: kIsLoggedIn)
+                    lougout()
+                    presentation.wrappedValue.dismiss()
+                }
                 ) {
-                Text("Log out")
-                    .font(.system(size: 15))
-                    .fontWeight(.bold)
-                    .foregroundColor(Color.black)
-            }
+                    Text("Log out")
+                        .font(.system(size: 15))
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.black)
+                }
                 .frame(maxWidth: .infinity, alignment: .center)
                 .frame(height: 40)
                 .background(Color.yellow)
                 .cornerRadius(8)
                 .padding(.top, 20)
                 .padding(.bottom, 20)
-            HStack{
-                Button(action: {
-                    discardChanges()
-                }) {
-                    Text("Discard changes")
-                        .font(.system(size: 16, weight: .medium))
-                        .foregroundColor(selectedChange ? Color.white : Color(red: 0.286, green: 0.369, blue: 0.341))
-                        .padding(.vertical, 8)
-                        .padding(.horizontal, 16)
-                        .background(
-                            RoundedRectangle(cornerRadius: 8)
-                                .fill(selectedChange ? Color(red: 0.286, green: 0.369, blue: 0.341) /*charcoal*/ : Color.white)
-                                .stroke(Color(red: 0.286, green: 0.369, blue: 0.341), lineWidth: 1)
-                        )
-                }
-                .padding(.leading, 12)
-                
-                Button(action: {
-                    saveChanges()
+                HStack{
+                    Button(action: {
+                        discardChanges()
+                    }) {
+                        Text("Discard changes")
+                            .font(.system(size: 16, weight: .medium))
+                            .foregroundColor(selectedChange ? Color.white : Color(red: 0.286, green: 0.369, blue: 0.341))
+                            .padding(.vertical, 8)
+                            .padding(.horizontal, 16)
+                            .background(
+                                RoundedRectangle(cornerRadius: 8)
+                                    .fill(selectedChange ? Color(red: 0.286, green: 0.369, blue: 0.341) /*charcoal*/ : Color.white)
+                                    .stroke(Color(red: 0.286, green: 0.369, blue: 0.341), lineWidth: 1)
+                            )
+                    }
+                    .padding(.leading, 12)
                     
-                }) {
-                    Text("Save changes")
-                        .font(.system(size: 16, weight: .medium))
+                    Button(action: {
+                        saveChanges()
+                        
+                    }) {
+                        Text("Save changes")
+                            .font(.system(size: 16, weight: .medium))
                         //.foregroundColor(selectedChange ? Color.white : Color(red: 0.286, green: 0.369, blue: 0.341))
-                        .foregroundColor(Color.white)
-                        .padding(.vertical, 8)
-                        .padding(.horizontal, 16)
-                        .background(
-                            RoundedRectangle(cornerRadius: 8)
+                            .foregroundColor(Color.white)
+                            .padding(.vertical, 8)
+                            .padding(.horizontal, 16)
+                            .background(
+                                RoundedRectangle(cornerRadius: 8)
                                 //.fill(selectedChange ? Color(red: 0.286, green: 0.369, blue: 0.341) /*charcoal*/ : Color.white)
-                                .fill(Color(red: 0.286, green: 0.369, blue: 0.341) /*charcoal*/)
-                                .stroke(Color(red: 0.286, green: 0.369, blue: 0.341), lineWidth: 1)
-                        )
+                                    .fill(Color(red: 0.286, green: 0.369, blue: 0.341) /*charcoal*/)
+                                    .stroke(Color(red: 0.286, green: 0.369, blue: 0.341), lineWidth: 1)
+                            )
+                    }
+                    .padding(.leading, 12)
                 }
-                .padding(.leading, 12)
+                //Spacer()
             }
-            //Spacer()
+            .padding(.horizontal, 20)
         }
-        .padding(.horizontal, 20)
     }
     
     func saveChanges() {
